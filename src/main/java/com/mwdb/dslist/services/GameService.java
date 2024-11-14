81f00332e40ace3f83679b0178d6d6738e71e3d1
MatheusWDB <matheuswdb1703@gmail.com>
2024-11-14 13:36:52 -0300

    Consulta SQL, projection, busca de games por lista

--- src/main/java/com/mwdb/dslist/services/GameService.java
+++ src/main/java/com/mwdb/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.mwdb.dslist.dto.GameDTO;
 import com.mwdb.dslist.dto.GameMinDTO;
 import com.mwdb.dslist.entities.Game;
+import com.mwdb.dslist.projections.GameMinProjection;
 import com.mwdb.dslist.repositories.GameRepository;
 
 @Service
@@ -24,8 +25,14 @@ public class GameService {
     }
 
     @Transactional(readOnly = true)
-    public GameDTO findById(Long id){
+    public GameDTO findById(Long id) {
         Game result = this.gameRepository.findById(id).get();
         return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }

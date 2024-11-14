edac21f5436d0949a56082247719f405b0cbca6c
MatheusWDB <matheuswdb1703@gmail.com>
2024-11-14 17:04:48 -0300

    Move games on list

---
+++ src/main/java/com/mwdb/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,23 @@
+package com.mwdb.dslist.dto;
+
+public class ReplacementDTO {
+
+    private Integer sourceIndex;
+    private Integer destinatioIndex;
+
+    public Integer getSourceIndex() {
+        return sourceIndex;
+    }
+
+    public void setSourceIndex(Integer sourceIndex) {
+        this.sourceIndex = sourceIndex;
+    }
+
+    public Integer getDestinatioIndex() {
+        return destinatioIndex;
+    }
+
+    public void setDestinatioIndex(Integer destinatioIndex) {
+        this.destinatioIndex = destinatioIndex;
+    }
+}
